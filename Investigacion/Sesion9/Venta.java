6facd6c0a4c6dce2ca8cd811bae7148142a9da56
Andres Vargas <a.m.vargas1996@gmail.com>
2022-03-16 21:49:53 -0300

    Clase sesion 9 (avance de actividad)

---
+++ Investigacion/Sesion9/Venta.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.Calendar;
+
+public class Venta {
+
+	private Calendar fecha = Calendar.getInstance();
+	private ArrayList<LineaDetalle> lineasDetalle = new ArrayList<>();
+	
+	
+	public Venta() {
+		
+	}
+	public Venta(Calendar fecha, ArrayList<LineaDetalle> lineasDetalle) {
+		this.fecha = fecha;
+		this.lineasDetalle = lineasDetalle;
+	}
+	
+	
+	public Calendar getFecha() {
+		return fecha;
+	}
+	public void setFecha(Calendar fecha) {
+		this.fecha = fecha;
+	}
+	public ArrayList<LineaDetalle> getLineasDetalle() {
+		return lineasDetalle;
+	}
+	public void setLineasDetalle(ArrayList<LineaDetalle> lineasDetalle) {
+		this.lineasDetalle = lineasDetalle;
+	}
+	
+	
+	
+	public void agregarLineaDetalle(LineaDetalle lineaDetalle) {
+		this.lineasDetalle.add(lineaDetalle);
+	}
+	
+	
+	
+}
