f1939fd1ee34031436499d3d3771a74fc2447182
Andres Vargas <a.m.vargas1996@gmail.com>
2022-04-12 19:41:14 -0300

    Sesion 31: Javadoc

---
+++ Investigacion/Sesion31/Documentacion/doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"facturacion","c":"Factura","l":"Factura(String, int)","u":"%3Cinit%3E(java.lang.String,int)"}];updateSearchResults();
---
+++ Investigacion/Sesion31/Documentacion/src/facturacion/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Este paquete contiene las clases necesarias para la facturacion electronica de las ventas.
+ */
+package facturacion;
